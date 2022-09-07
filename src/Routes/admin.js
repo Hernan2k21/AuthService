1626cde1528299d51b7262248b77626377a1cc46
Hernan2k21 <hernan@genosha.com.ar>
2022-09-07 11:41:06 -0300

    remove unnecesary path param

--- src/Routes/admin.js
+++ src/Routes/admin.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../Controllers/authController')
 
-router.post('/blacklist-refresh/user/:id', authController.blacklistRefreshToken)
+router.post('/blacklist-refresh/user', authController.blacklistRefreshToken)
 module.exports = router;
